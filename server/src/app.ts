ddbc60ef4b6368d9690be1c7af2bb75ffb23d82b
sopnilali <sopnilali24@gmail.com>
2025-07-20 03:47:12 +0600

    sdsd

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ const app = express()
 
 // CORS configuration
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173' ],
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://mini-event-scheduler-api.vercel.app' ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
