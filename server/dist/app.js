4f80ec73d7579fc40b4c662b782a2875ca0dd377
sopnilali <sopnilali24@gmail.com>
2025-07-20 03:57:04 +0600

    cors error fix

--- server/dist/app.js
+++ server/dist/app.js
@@ -12,7 +12,7 @@ const globalErrorHandler_1 = __importDefault(require("./app/middleware/globalErr
 const app = (0, express_1.default)();
 // CORS configuration
 app.use((0, cors_1.default)({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://mini-event-scheduler-api.vercel.app'],
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://mini-event-scheduler-api.vercel.app', 'https://mini-event-schedulerui.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ const app = express()
 
 // CORS configuration
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://mini-event-scheduler-api.vercel.app' ],
+    origin: ['http://localhost:3000', 'http://localhost:5173', 'https://mini-event-scheduler-api.vercel.app', 'https://mini-event-schedulerui.vercel.app' ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
